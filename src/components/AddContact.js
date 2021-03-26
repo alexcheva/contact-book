9e827bedf4a15229da23898da0ec671e0a990e5f
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-26 11:39:44 -0700

    change App.js, add components

---
+++ src/components/AddContact.js
@@ -0,0 +1,26 @@
+const AddContactForm = () => {
+  return (
+    <div>
+      <h2>Add Contact</h2>
+      <form>
+        <fieldset>
+          <label for="fname">First name:</label>
+          <input type="text" name="fname" />
+        </fieldset>
+        <fieldset>
+          <label for="lname">Last name:</label>
+          <input type="text" name="lname" />
+        </fieldset>
+        <fieldset>
+          <label for="phone_num">Phone Number:</label>
+          <input type="number" name="phone_num" />
+        </fieldset>
+        <fieldset>
+          <label for="email">Email:</label>
+          <input type="email" name="email" />
+        </fieldset>
+        <input type="submit" value="Add Contact" />
+      </form>
+    </div>
+  );
+}
